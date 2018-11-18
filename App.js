d87e556e3aa023401f86d4ab6774ad7d495678e8
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2018-11-18 22:31:24 +0200

    finish animations

--- App.js
+++ App.js
@@ -3,20 +3,61 @@ import { StyleSheet, View, Button, Animated } from "react-native";
 
 export default class App extends Component {
 	state = {
-		opacity: new Animated.Value(1)
+		opacity: new Animated.Value(1),
+		opacity2: new Animated.Value(0)
 	};
+
+	animatedViewTwoHandler = () => {
+		Animated.timing(this.state.opacity2, {
+			duration: 500,
+			toValue: 1,
+			useNativeDriver: true
+		}).start();
+	};
+
 	clickHandler = () => {
 		Animated.timing(this.state.opacity, {
 			duration: 500,
 			toValue: 0,
 			useNativeDriver: true
-		}).start();
+		}).start(() => {
+			this.animatedViewTwoHandler();
+		});
 	};
 	render() {
 		return (
 			<View style={styles.container}>
 				<Animated.View
-					style={[styles.animatedView, { opacity: this.state.opacity }]}
+					style={[
+						styles.animatedView,
+						{
+							opacity: this.state.opacity,
+							transform: [
+								{
+									scale: this.state.opacity.interpolate({
+										inputRange: [0, 1],
+										outputRange: [12, 1]
+									})
+								}
+							]
+						}
+					]}
+				/>
+				<Animated.View
+					style={[
+						styles.animatedViewTwo,
+						{
+							opacity: this.state.opacity2,
+							transform: [
+								{
+									scale: this.state.opacity2.interpolate({
+										inputRange: [0, 1],
+										outputRange: [1, 0]
+									})
+								}
+							]
+						}
+					]}
 				/>
 				<Button title="Click Me" onPress={this.clickHandler} />
 			</View>
@@ -36,5 +77,11 @@ const styles = StyleSheet.create({
 		height: 200,
 		backgroundColor: "red",
 		marginBottom: 50
+	},
+	animatedViewTwo: {
+		width: "100%",
+		height: "100%",
+		backgroundColor: "green",
+		position: "absolute"
 	}
 });
