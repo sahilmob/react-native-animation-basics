df30d066bbd534b8b24891b43ad2c10eeee57492
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2018-11-18 22:22:01 +0200

    finish opacity animation

--- App.js
+++ App.js
@@ -1,21 +1,40 @@
-import React, {Component} from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import React, { Component } from "react";
+import { StyleSheet, View, Button, Animated } from "react-native";
 
-export default class App extends Component{
-  render() {
-    return (
-      <View style={styles.container}>
-        
-      </View>
-    );
-  }
+export default class App extends Component {
+	state = {
+		opacity: new Animated.Value(1)
+	};
+	clickHandler = () => {
+		Animated.timing(this.state.opacity, {
+			duration: 500,
+			toValue: 0,
+			useNativeDriver: true
+		}).start();
+	};
+	render() {
+		return (
+			<View style={styles.container}>
+				<Animated.View
+					style={[styles.animatedView, { opacity: this.state.opacity }]}
+				/>
+				<Button title="Click Me" onPress={this.clickHandler} />
+			</View>
+		);
+	}
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  }
+	container: {
+		flex: 1,
+		justifyContent: "center",
+		alignItems: "center",
+		backgroundColor: "#F5FCFF"
+	},
+	animatedView: {
+		width: "50%",
+		height: 200,
+		backgroundColor: "red",
+		marginBottom: 50
+	}
 });
